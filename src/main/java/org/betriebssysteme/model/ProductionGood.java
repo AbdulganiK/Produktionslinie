8524a15711de871ab278b458290cf44540cbf07f
AbdulganiK <abdulganikorkmaz589@gmail.com>
2025-12-21 15:28:32 +0100

    Enum hinzugefuegt fuer productiongueter tix-11

---
+++ src/main/java/org/betriebssysteme/model/ProductionGood.java
@@ -0,0 +1,11 @@
+package org.betriebssysteme.model;
+
+public enum ProductionGood {
+    DRIVE_HOUSING,
+    CONTROL_HOUSING,
+    DRIVE_PCB,
+    CONTROL_PCB,
+    DRIVE_UNIT,
+    CONTROL_UNIT,
+    SHIPPING_PACKAGE
+}
