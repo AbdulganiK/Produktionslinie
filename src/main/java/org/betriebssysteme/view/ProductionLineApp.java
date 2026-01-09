b211931734ae79c6fc46585fef1a61eeebd846a8
AbdulganiK <abdulganikorkmaz589@gmail.com>
2026-01-09 16:23:06 +0100

    menu text fix

--- src/main/java/org/betriebssysteme/view/ProductionLineApp.java
+++ src/main/java/org/betriebssysteme/view/ProductionLineApp.java
@@ -8,6 +8,8 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.GameWorld;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import javafx.geometry.Point2D;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
@@ -15,12 +17,15 @@ import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 public class ProductionLineApp extends GameApplication {
 
     Zoom zoom = new Zoom();
+    private Point2D lastDragPos;
+
 
 
     @Override
     protected void initInput() {
         this.zoom.initResetZoom();
         this.zoom.initZoomToMouse();
+        this.initCameraDrag();
     }
 
 
@@ -39,6 +44,7 @@ public class ProductionLineApp extends GameApplication {
         FXGL.getPhysicsWorld().setGravity(0, 0);
     }
 
+
     @Override
     protected void initGame() {
         // hinzufuegen der Entity Fabrik
@@ -48,6 +54,45 @@ public class ProductionLineApp extends GameApplication {
 
         Entity machine2 = FXGL.spawn(EntityNames.MACHINE, 400, 300);
 
+    }
+
+    private void initCameraDrag() {
+        var scene = FXGL.getPrimaryStage().getScene();
+        Viewport vp = FXGL.getGameScene().getViewport();
+
+        // Maus runter -> Startpunkt merken
+        scene.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
+            if (e.getButton() == MouseButton.MIDDLE) {   // oder PRIMARY / SECONDARY, wie du willst
+                lastDragPos = new Point2D(e.getSceneX(), e.getSceneY());
+            }
+        });
+
+        // Maus ziehen -> Kamera bewegen
+        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, e -> {
+            if (e.isMiddleButtonDown() && lastDragPos != null) {
+
+                double dxScreen = e.getSceneX() - lastDragPos.getX();
+                double dyScreen = e.getSceneY() - lastDragPos.getY();
+
+                double zoom = vp.getZoom();
+
+                // Screen-Pixel in Weltverschiebung umrechnen
+                double dxWorld = -dxScreen / zoom;
+                double dyWorld = -dyScreen / zoom;
+
+                vp.setX(vp.getX() + dxWorld);
+                vp.setY(vp.getY() + dyWorld);
+
+                lastDragPos = new Point2D(e.getSceneX(), e.getSceneY());
+            }
+        });
+
+        // Maus loslassen -> Drag beenden
+        scene.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
+            if (e.getButton() == MouseButton.MIDDLE) {
+                lastDragPos = null;
+            }
+        });
     }
 
 
