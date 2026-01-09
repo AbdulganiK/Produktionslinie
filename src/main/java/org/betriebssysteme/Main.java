962db5ff6ca740b1f30da9f3e6a5dad470d40aba
S-Schult <Jonas.Schult@stud.hs-bremen.de>
2026-01-09 16:04:35 +0100

    add logging configuration and update Main class for production line simulation

--- src/main/java/org/betriebssysteme/Main.java
+++ src/main/java/org/betriebssysteme/Main.java
@@ -1,9 +1,12 @@
 package org.betriebssysteme;
+import org.slf4j.Logger;
 
 import org.betriebssysteme.control.ProductionController;
 
 public class Main {
     public static void main(String[] args) {
+        Logger logger = org.slf4j.LoggerFactory.getLogger(Main.class);
+        logger.info("Starting Production Line Simulation");
         ProductionController productionController = new ProductionController();
         productionController.createAllStations();
         productionController.createAllPersonnel();
