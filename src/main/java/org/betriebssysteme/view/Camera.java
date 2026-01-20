812335c7aafe4ead529df11fb5ba5ef91b7b515f
S-Schult <Jonas.Schult@stud.hs-bremen.de>
2026-01-20 17:06:53 +0100

    Sorry habe gerade keine Maus

--- src/main/java/org/betriebssysteme/view/Camera.java
+++ src/main/java/org/betriebssysteme/view/Camera.java
@@ -5,6 +5,7 @@ import com.almasb.fxgl.dsl.FXGL;
 import javafx.geometry.Point2D;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import jdk.jfr.Event;
 
 public class Camera {
     private Point2D lastDragPos;
@@ -15,6 +16,9 @@ public class Camera {
 
         scene.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
             if (e.getButton() == MouseButton.MIDDLE) {   // oder PRIMARY / SECONDARY, wie du willst
+                lastDragPos = new Point2D(e.getSceneX(), e.getSceneY());
+            }
+            if (e.getButton() == MouseButton.SECONDARY) {
                 lastDragPos = new Point2D(e.getSceneX(), e.getSceneY());
             }
         });
@@ -36,10 +40,29 @@ public class Camera {
 
                 lastDragPos = new Point2D(e.getSceneX(), e.getSceneY());
             }
+            if (e.isSecondaryButtonDown() && lastDragPos != null) {
+
+                double dxScreen = e.getSceneX() - lastDragPos.getX();
+                double dyScreen = e.getSceneY() - lastDragPos.getY();
+
+                double zoom = vp.getZoom();
+
+                // Screen-Pixel in Weltverschiebung umrechnen
+                double dxWorld = -dxScreen / zoom;
+                double dyWorld = -dyScreen / zoom;
+
+                vp.setX(vp.getX() + dxWorld);
+                vp.setY(vp.getY() + dyWorld);
+
+                lastDragPos = new Point2D(e.getSceneX(), e.getSceneY());
+            }
         });
 
         scene.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
             if (e.getButton() == MouseButton.MIDDLE) {
+                lastDragPos = null;
+            }
+            if (e.getButton() == MouseButton.SECONDARY) {
                 lastDragPos = null;
             }
         });
